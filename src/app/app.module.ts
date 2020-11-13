29a8780c093fe850006b76d5d522086f40836ad9
Mandy Blaschke <post@mandy-blaschke.de>
2020-11-13 14:24:09 +0100

    edited: styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { ButtonComponent } from './button/button.component';
 import { HiddenCardComponent } from './hidden-card/hidden-card.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
     ButtonComponent,
-    HiddenCardComponent
+    HiddenCardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
